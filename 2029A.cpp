cf83249cdcb6242cae280592db7c020548f10d8d
SamratEmily <nayakemily50@gmail.com>
2024-11-13 18:41:37 +0600

    updated

--- 2029A.cpp
+++ 2029A.cpp
@@ -10,26 +10,25 @@ int main()
     {
         int l, r, k;
         cin >> l >> r >> k;
-        int s = l, e = r;
-        while (l < r)
-        {
-            int mid = (l + r + 1) / 2;
-            int op = e / mid;
+        // int s = l, e = r;
+        // while (l < r)
+        // {
+        //     int mid = (l + r + 1) / 2;
+        //     int op = e / mid;
 
-            if (op >= k)
-                l = mid;
-            else
-                r = mid - 1;
-        }
-        if (l - s == 0)
+        //     if (op >= k)
+        //         l = mid;
+        //     else
+        //         r = mid - 1;
+        // }
+
+        int ans = (r / k) - l + 1;
+        if (ans <= 0)
         {
-            if (e / l >= k)
-                cout << 1 << endl;
-            else
-                cout << 0 << endl;
+            cout << 0 << endl;
         }
         else
-            cout << (l - s + 1) << endl;
+            cout << ans << endl;
     }
 
     return 0;
