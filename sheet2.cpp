b67e020e6e319b15edcf63a46cf1fd4a0f211b76
SamratEmily <nayakemily50@gmail.com>
2025-05-22 10:06:20 +0600

    solved sheet 2 last 3problems

--- sheet2.cpp
+++ sheet2.cpp
@@ -1,31 +1,28 @@
 #include <iostream>
- 
-using namespace std;
- 
-int main() {
-    int n, a, b;
-    cin >> n;
- 
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= (n-i); j++) cout << " ";
-        for (int j = 1; j <= i; j++) {
-            cout << "*";
-        }
-        for (int j = 2; j <= i; j++) {
-            cout << "*";
-        }
-        cout << endl;
-    }
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= (i-1); j++) cout << " ";
-        for (int j = 1; j <= n-i+1; j++) {
-            cout << "*";
-        }
-        for (int j = 2; j <= n-i+1; j++) {
-            cout << "*";
-        }
-        cout << endl;
-    }
     
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long n;
+        cin >> n;
+
+        long long cntOne = 0;
+        while (n > 0)
+        {
+            if(n % 2 == 1) cntOne++;
+            n /= 2;
+        }
+
+        long long result = 0;
+        for(long long i = 0; i < cntOne; i++) {
+            result = (result << 1) | 1;
+        }
+        cout << result << endl;
+    }
+        
     return 0;
 }
